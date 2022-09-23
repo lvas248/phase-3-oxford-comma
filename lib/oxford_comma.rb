9dd6cb0f5e64827af99d6474bc28c7f2f5386101
Luis <lvas248@gmail.com>
2022-09-23 17:08:43 -0400

    commit tree 89ae7572b45c5d60a8713abeff7b0467d6d037bf

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length < 2
+        array.join
+    elsif array.length == 2
+        array.join(' and ')
+    else
+        temp = array.last
+        array[array.length-1] = "and #{temp}"
+        string = array.join(', ')
+        string
+    end
 end
+
+oxford_comma(["Chimi"])
